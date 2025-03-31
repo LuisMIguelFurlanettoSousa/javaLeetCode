8da5ff6a379aa090bc5a22a52e5d5175f5600398
Luís Miguel <148079567+LuisMIguelFurlanettoSousa@users.noreply.github.com>
2025-03-31 17:51:21 -0300

    up

---
+++ src/Easy/ReverseLinkedList.java
@@ -0,0 +1,29 @@
+package src.Easy;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode nextTemp = current.next; // Guarda o próximo nó
+            current.next = prev;  // Inverte a ligação
+            prev = current;  // Move `prev` para frente
+            current = nextTemp; // Move `current` para frente
+        }
+
+        return prev; // Novo head da lista invertida
+    }
+}
