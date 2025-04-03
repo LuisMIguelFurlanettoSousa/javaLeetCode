3a78c89d2e74f0dda5d86437d8f309c7094c7ae9
Luís Miguel <148079567+LuisMIguelFurlanettoSousa@users.noreply.github.com>
2025-04-03 14:33:25 -0300

    tentativa

--- src/Mediun/LongestSubstringWithoutRepeatingCharacters.java
+++ src/Mediun/LongestSubstringWithoutRepeatingCharacters.java
@@ -4,11 +4,21 @@ import java.util.Set;
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         Set<Character> caracteres = new HashSet<>();
+        int left = 0, maxLength = 0;
 
-        for (char letra : s) {
+        for (int right = 0; right < s.length(); right++) {
+            char letra = s.charAt(right);
+
+            // Se a letra já estiver no conjunto, removemos caracteres do início até que ela saia
+            while (caracteres.contains(letra)) {
+                caracteres.remove(s.charAt(left));
+                left++;
+            }
+
             caracteres.add(letra);
+            maxLength = Math.max(maxLength, right - left + 1);
         }
 
-        return caracteres.size();
+        return maxLength;
     }
 }
